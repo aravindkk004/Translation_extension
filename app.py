0f937d52ef02f4b186e8761b5e8751ccfc41cfe2
aravindkk004 <aravind463kk30@gmail.com>
2024-05-25 22:24:54 +0530

    final commit

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 from googletrans import Translator
 import json
 from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)  
@@ -18,4 +22,5 @@ def translate():
     return jsonify({'error': 'Invalid input'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))  # Default to port 10000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
